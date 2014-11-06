d0cd1b7430063d62547d52e7036a346ead1c79d7
Mario Sanchez Prada <mario@endlessm.com>
2014-11-06 15:53:45 +0000

    unzip_content.py: allow moving apps to the end of their categories
    This is a hack while issue shell/3655 is not fixed, to allow moving
    certain apps to the end of the generated content.json file, which will
    effectively cause them to show up at the end of their respective
    categories in the app store.
    
    [endlessm/eos-shell#3674]

--- unzip_content.py
+++ unzip_content.py
@@ -44,6 +44,11 @@ JPEG_QUALITY = 90
 APP_PREFIX = 'eos-app-'
 LINK_PREFIX = 'eos-link-'
 
+# Hack: apps we want to show up at the end of their category in the app store
+# The long term solution involving the CMS will be addressed on #3655
+# https://github.com/endlessm/eos-shell/issues/3674
+APPS_TO_APPEND = []
+
 # Run the ImageMagick 'convert' application from the command line,
 # with specified JPEG quality and all metadata stripped
 def convert(source, target, command):
@@ -119,6 +124,21 @@ if __name__ == '__main__':
     with open(target) as infile:
         json_data = json.load(infile)
     sorted_json = sorted(json_data, key=operator.itemgetter('application-id'))
+
+    # Hack: modify the sorted list moving to the end all
+    # the apps that we want to show up at the end of their
+    # respective categories in the app store
+    app_index = 0
+    apps_to_append = []
+    while app_index < len(sorted_json):
+        app_data = sorted_json[app_index]
+        if app_data['application-id'] in APPS_TO_APPEND:
+            # Remove it from the original one and save it
+            apps_to_append.append(sorted_json.pop(app_index))
+            continue;
+        app_index += 1
+    sorted_json.extend(apps_to_append)
+
     with open(target, 'w') as outfile:
         json.dump(sorted_json, outfile, indent=2, sort_keys=True)
 
