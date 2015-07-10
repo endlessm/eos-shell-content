389d782c4d4331f4da01fafc23bb2f9022babb66
Dan Nicholson <nicholson@endlessm.com>
2015-07-10 15:05:52 -0700

    Convert yelp for content merging
    [endlessm/eos-shell#5272]

--- desktop_object.py
+++ desktop_object.py
@@ -11,7 +11,8 @@ MIME_TYPES = {
 # in /usr/share/applications.
 CORE_CONVERTED = ['gnome-terminal', 'chromium-browser', 'eos-file-manager',
                   'gnome-control-center', 'libreoffice-calc',
-                  'libreoffice-impress', 'libreoffice-writer', 'rhythmbox']
+                  'libreoffice-impress', 'libreoffice-writer', 'rhythmbox',
+                  'yelp']
 
 class DesktopObject(object):
 
