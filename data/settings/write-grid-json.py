bf9451649bbef005e88afe14572018a7fea7c155
Dan Nicholson <nicholson@endlessm.com>
2015-11-16 15:35:52 -0800

    data: Allow blacklisting of icon grid entries by arch
    Some icon grid entries may not be appropriate on specific architectures,
    so they need to be stripped from the final json files. To handle this, a
    separate arch-blacklist.json is provided which can list entries that
    should be blacklisted. A script, write-grid-json.py, is added to parse
    the lists and remove the relevant entries.
    
    The blacklist initially removes spotify on ARM since this requires
    flash.
    
    [endlessm/eos-shell#6012]

---
+++ data/settings/write-grid-json.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+from argparse import ArgumentParser
+from collections import OrderedDict
+import errno
+import json
+import os
+import sys
+
+aparser = ArgumentParser(description='Write icon grid json file')
+aparser.add_argument('-o', '--output', help='output file')
+aparser.add_argument('input', help='input template file')
+aparser.add_argument('blacklist', help='blacklist file')
+aparser.add_argument('cpu', help='CPU for blacklisting')
+args = aparser.parse_args()
+
+# Load with template and blacklist. Use OrderedDict for the grid to
+# maintain sorting of the keys.
+with open(args.input, 'r') as infile:
+    grid = json.load(infile, object_pairs_hook=OrderedDict)
+with open(args.blacklist, 'r') as blfile:
+    blacklist = json.load(blfile)
+
+# Open the output file if specified, ensuring that leading directories
+# are created first.
+if args.output is None:
+    outfile = sys.stdout
+else:
+    outdir = os.path.dirname(args.output)
+    try:
+        os.makedirs(outdir)
+    except OSError as err:
+        if err.errno != errno.EEXIST:
+            raise
+    outfile = open(args.output, 'w')
+
+# Strip out blacklisted apps
+cpu_blacklist = blacklist.get(args.cpu, [])
+for app in cpu_blacklist:
+    for sect, apps in grid.items():
+        if app in apps:
+            grid[sect].remove(app)
+
+# Write out the new json, keeping the indentation, separators and
+# trailing newline from the original.
+json.dump(grid, outfile, indent=2, separators=(',', ' : '))
+outfile.write('\n')
